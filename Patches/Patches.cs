96df0a8b400d4901d1b84a616f495657e394138f
Utjan <utjan90@hotmail.com>
2025-12-30 00:30:24 +0100

    1.0

---
+++ Patches/Patches.cs
@@ -0,0 +1,32 @@
+﻿using EFT;
+using EFT.Hideout;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace AirFilterQOLClientMod.Patches
+{
+    internal class AirfilterUpdatePatch : ModulePatch // all patches must inherit ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(AirFilteringUnitBehaviour), nameof(AirFilteringUnitBehaviour.Update));
+        }
+
+
+        [PatchPrefix]
+        static bool Prefix(ref AirFilteringUnitBehaviour __instance, float deltaTime, out bool __state)
+        {
+            __state = __instance.ResourceConsumer.IsOn;            
+            __instance.ResourceConsumer.IsOn = false;
+
+            return true; // return true to run the original code
+        }
+
+        [PatchPostfix]
+        static void Postfix(ref AirFilteringUnitBehaviour __instance, bool __state)
+        {
+            __instance.ResourceConsumer.IsOn = __state;
+        }
+    }
+}
